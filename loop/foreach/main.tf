523e2e8921106b64cb1510d01759e94dc35ac32d
chinnapareddy <chinna.aduri@gmail.com>
2023-08-11 01:07:10 -0700

    terraform

--- loop/foreach/main.tf
+++ loop/foreach/main.tf
@@ -1,12 +1,25 @@
-variable "components"{
+#variable "components"{
+#  default = {
+#    frontend = { name = "frontend_sg"}
+#    catalogue = { name = "catalogue_sg"}
+#    mongodb = { name = "mongodb_sg"}
+#  }
+#}
+#
+#resource "aws_security_group" "allow-tls" {
+#  for_each = var.components
+#  name = lookup(var.components, each.value["name"], null)
+#}
+
+variable "components" {
   default = {
-    frontend = { name = "frontend_sg"}
-    catalogue = { name = "catalogue_sg"}
-    mongodb = { name = "mongodb_sg"}
+    catalogue = { name = "catalogue_sg" }
+    mongodb = { name = "mongodb_sg"  }
   }
 }
 
-resource "aws_security_group" "allow-tls" {
+
+resource "aws_security_group" "allow_tls" {
   for_each = var.components
-  name = lookup(var.components, each.value["name"], null)
+  name        = lookup(var.components, each.value["name"], null)
 }
