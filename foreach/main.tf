32fd07f2707ec0bba4c077d2fd637493ba1c7e8d
chinnapareddy <chinna.aduri@gmail.com>
2023-08-10 22:39:28 -0700

    loops

--- foreach/main.tf
+++ foreach/main.tf
@@ -5,7 +5,7 @@ variable "components"{
   }
 }
 
-resource "aws_security_group" "allow-all" {
+resource "aws_security_group" "allow-tls" {
   for_each = var.components
   name = lookup(var.components, each.value["name"], null)
 }
