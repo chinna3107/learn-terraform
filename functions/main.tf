e3409b9bd13d309a83e7e384662f9ce26a5f37da
chinnapareddy <chinna.aduri@gmail.com>
2023-08-10 20:21:55 -0700

    functions

--- functions/main.tf
+++ functions/main.tf
@@ -6,3 +6,13 @@ output "fruits" {
   //value = var.fruit[2]
   value = element(var.fruit, 2)
 }
+
+variable "fruits_with_stock" {
+default = {
+  apple = 100
+}
+}
+
+output "fruit_stock" {
+  value = try(var.fruits_with_stock["banana"], 0)
+}
