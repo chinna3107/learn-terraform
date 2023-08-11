f0d03878c6733e5b1cb2f4d10d3187453392226b
chinnapareddy <chinna.aduri@gmail.com>
2023-08-10 19:52:30 -0700

    Roboshop Server creation

--- roboshopv1/roboshop.tf
+++ roboshopv1/roboshop.tf
@@ -1,7 +1,22 @@
+variable "ami" {
+  default = "ami-0dd64e87afd183e02"
+}
+
+variable "instance_type"{
+  default : "t3.micro"
+}
+
+variable "Security_group" {
+  default = [ "sg-0718583b9b8a75cc2" ]
+}
+variable "zone_id" {
+  default = "Z02130323PCBS9CDUOI7M"
+}
+
 resource "aws_instance" "frontend" {
-  ami           = "ami-0dd64e87afd183e02"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [ "sg-0718583b9b8a75cc2" ]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.Security_group
 
   tags = {
     Name = "frontend"
@@ -9,7 +24,7 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z02130323PCBS9CDUOI7M"
+  zone_id = var.zone_id
   name    = "frontend-dev.devops-tools.online"
   type    = "A"
   ttl     = 30
