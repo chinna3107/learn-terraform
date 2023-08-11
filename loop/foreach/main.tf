3feed252a82146a0ba23b60b8b388760a609e03c
chinnapareddy <chinna.aduri@gmail.com>
2023-08-11 04:48:23 -0700

    terraform

--- loop/foreach/main.tf
+++ loop/foreach/main.tf
@@ -23,6 +23,6 @@ resource "aws_security_group" "allow_tls" {
   for_each = var.components
   name        = lookup(var.components, each.value["name"], null)
   tags = {
-    Name = "name"
+    Name = var.components
   }
 }
