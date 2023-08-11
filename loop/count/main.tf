a2355fec74c2cd0eefe6cc6def41e64360acbecb
chinnapareddy <chinna.aduri@gmail.com>
2023-08-10 23:17:05 -0700

    loops

---
+++ loop/count/main.tf
@@ -0,0 +1,17 @@
+variable "component" {
+  default = ["frontend", "mongodb"]
+}
+
+resource "aws_instance" "instance" {
+
+  count = 2
+
+  ami = "ami-0dd64e87afd183e02"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0718583b9b8a75cc2" ]
+
+  tag =   {
+    name = ""
+  }
+
+}
