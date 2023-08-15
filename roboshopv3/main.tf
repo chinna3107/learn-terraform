4e97837139367058a5e7d6d7a9a0e3ac0f4c5b09
chinnapareddy <chinna.aduri@gmail.com>
2023-08-15 00:17:13 -0700

    module roboshop

--- roboshopv3/main.tf
+++ roboshopv3/main.tf
@@ -2,10 +2,10 @@ module "component" {
 
   for_each = var.component
 
-  source           = " ./module"
-  zone_id          = var.zone_id
-  security_group   = var.Security_group
-  name             = each.value["name"]
-  instance_type    = each.value["instance_type"]
+  source         = "./module"
+  zone_id        = var.zone_id
+  security_group = var.Security_group
+  name           = each.value["name"]
+  instance_type  = each.value["instance_type"]
 
 }
