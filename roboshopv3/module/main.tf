25bc0af653c12746c97b77f53c4b70ff3defd029
chinnapareddy <chinna.aduri@gmail.com>
2023-08-15 02:46:02 -0700

    roboshop terraform ansible

--- roboshopv3/module/main.tf
+++ roboshopv3/module/main.tf
@@ -13,7 +13,7 @@ resource "aws_route53_record" "record" {
   name    = "${var.name}-dev.devops-tools.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.instance.private_ip ]
+  records = [aws_instance.instance.private_ip]
 }
 
 resource "null_resource"  ansible {
@@ -26,9 +26,8 @@ resource "null_resource"  ansible {
 cd /root/roboshop-ansible1
 git pull
 sleep 30
-ansible-playbook -i ${var.name}-dev.devops-tools.online", main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e
-component=${var.name}
-
+ansible-playbook -i ${var.name}-dev.devops-tools.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321
+-e component=${var.name}
 EOF
   }
 }
