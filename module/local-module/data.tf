5a807949500125d90eb937bf82930ab61547bdbb
chinnapareddy <chinna.aduri@gmail.com>
2023-08-14 22:03:53 -0700

    module

--- module/local-module/data.tf
+++ module/local-module/data.tf
@@ -1,15 +1,15 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
   #  most_recent      = true
   name_regex       = "backup"
   owners           = ["471757984210"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
 
 resource "aws_instance" "instance" {
-  ami = data.aws_ami_ids.ami
+  ami = data.aws_ami.ami.id
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-0718583b9b8a75cc2" ]
 }
