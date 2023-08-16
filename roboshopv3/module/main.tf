dbc05132b1ec2a1c4cc3e95994e20f65b3ea2031
chinnapareddy <chinna.aduri@gmail.com>
2023-08-16 09:31:19 -0700

    learn terraform

--- roboshopv3/module/main.tf
+++ roboshopv3/module/main.tf
@@ -26,7 +26,7 @@ resource "null_resource"  "ansible" {
   cd /root/roboshop-ansible1
   git pull
   sleep 30
-  ansible-playbook -i ${var.name}-dev.devops-tools.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+  ansible-playbook -i ${var.name}.devops-tools.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
   EOF
   }
 }
