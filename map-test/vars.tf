47660e5f4c5da5770a2eafbcd1abe3e2d5b3f213
chinnapareddy <chinna.aduri@gmail.com>
2023-08-29 23:07:49 -0700

    subnet creation test

---
+++ map-test/vars.tf
@@ -0,0 +1,25 @@
+variable "vpc" {
+  default = {
+    main = {
+        cidr = "10.0.0.0/24"
+        subnets = {
+          public = {
+            public1 = { cidr = "10.0.0.0/24", az = "us-east-1a" }
+            public2 = { cidr = "10.0.1.0/24", az = "us-east-1a" }
+          }
+          app = {
+            app1 = { cidr = "10.0.2.0/24", az = "us-east-1a" }
+            app2 = { cidr = "10.0.3.0/24", az = "us-east-1a" }
+          }
+          db = {
+            db1 = { cidr = "10.0.4.0/24", az = "us-east-1a" }
+            db2 = { cidr = "10.0.5.0/24", az = "us-east-1a" }
+          }
+        }
+    }
+  }
+}
+
+output "all_subnet" {
+  value = var.vpc["main"]["subnets"]
+}
